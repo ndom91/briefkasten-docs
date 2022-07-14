e2893f63dfa9dfb0b56aed2239042c723c2c313f
ndom91 <yo@ndo.dev>
2022-07-14 20:39:31 +0200

    feat: Features + Footer sections

---
+++ docs/components/Features.vue
@@ -0,0 +1,132 @@
+<template>
+  <section class="m-4 dark:bg-gray-800 dark:text-gray-100 md:m-8">
+    <div class="container mx-auto my-6 space-y-2 p-4 text-center">
+      <h2 class="text-5xl font-bold">Built to empower every user</h2>
+      <p class="dark:text-gray-400">Always find what you're looking for!</p>
+    </div>
+    <div
+      class="container mx-auto grid justify-center gap-4 sm:grid-cols-2 lg:grid-cols-3"
+    >
+      <div class="flex flex-col items-center p-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          class="h-8 w-8 dark:text-slate-400"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <h3 class="my-3 text-3xl font-semibold">Self-Hostable</h3>
+        <div class="space-y-1 leading-tight">
+          <p>Similique quas ea veniam</p>
+          <p>Tempore quasi porro</p>
+          <p>Blanditiis aut mollitia ex</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center p-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          class="h-8 w-8 dark:text-slate-400"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <h3 class="my-3 text-3xl font-semibold">Browser Extension</h3>
+        <div class="space-y-1 leading-tight">
+          <p>Similique quas ea veniam</p>
+          <p>Tempore quasi porro</p>
+          <p>Blanditiis aut mollitia ex</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center p-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          class="h-8 w-8 dark:text-slate-400"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <h3 class="my-3 text-3xl font-semibold">Import/Export</h3>
+        <div class="space-y-1 leading-tight">
+          <p>Similique quas ea veniam</p>
+          <p>Tempore quasi porro</p>
+          <p>Blanditiis aut mollitia ex</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center p-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          class="h-8 w-8 dark:text-slate-400"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <h3 class="my-3 text-3xl font-semibold">Screenshot Fetching</h3>
+        <div class="space-y-1 leading-tight">
+          <p>Similique quas ea veniam</p>
+          <p>Tempore quasi porro</p>
+          <p>Blanditiis aut mollitia ex</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center p-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          class="h-8 w-8 dark:text-slate-400"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <h3 class="my-3 text-3xl font-semibold">Full-text Search</h3>
+        <div class="space-y-1 leading-tight">
+          <p>Similique quas ea veniam</p>
+          <p>Tempore quasi porro</p>
+          <p>Blanditiis aut mollitia ex</p>
+        </div>
+      </div>
+      <div class="flex flex-col items-center p-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          class="h-8 w-8 dark:text-slate-400"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <h3 class="my-3 text-3xl font-semibold">REST API</h3>
+        <div class="space-y-1 leading-tight">
+          <p>Similique quas ea veniam</p>
+          <p>Tempore quasi porro</p>
+          <p>Blanditiis aut mollitia ex</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- docs/index.md
+++ docs/index.md
@@ -3,18 +3,24 @@ layout: 'home'
 footer: false
 head:
   - - link
-    - href: "/favicon/favicon.ico"
-      rel: "shortcut icon"
+    - href: '/favicon/favicon.ico'
+      rel: 'shortcut icon'
   - - script
-    - src: "/p.js"
+    - src: '/p.js'
       defer: true
-      data-domain: "briefkasten-docs.vercel.app"
-      data-api: "/a/e"
+      data-domain: 'briefkasten-docs.vercel.app'
+      data-api: '/a/e'
 ---
 
 <script setup>
 import useData from './composables/useData.vue'
 import Hero from './components/Hero.vue'
+import Features from './components/Features.vue'
+import Footer from './components/Footer.vue'
 </script>
 
-<Hero />
+<div class="flex flex-col items-center justify-start h-screen">
+  <Hero />
+  <Features />
+  <Footer />
+</div>
