b05f7c9dbd02d9d60d246f72093009c720d6d490
ndom91 <yo@ndo.dev>
2022-08-21 21:40:41 +0200

    feat: vueuse/motion without SSR directives

--- docs/components/HeroImage.vue
+++ docs/components/HeroImage.vue
@@ -1,28 +1,37 @@
 <template>
   <div style="perspective: 600px" class="flex items-center justify-center">
-  <div class="relative -mt-32 w-full sm:w-4/5 md:w-3/5 z-50" ref="el" :style="imageStyle" v-motion
-    :initial="{ opacity: 0, scale: 0.5, y: 100 }" :enter="{
-      opacity: 1, y: 0, scale: 1, transition: {
-        delay: 1000, type: 'spring',
-        stiffness: 250,
-        damping: 25,
-        mass: 0.5,
-      }
-    }">
-    <img src="/images/screenshot_hero.png" alt="" class="" />
+    <div class="relative -mt-32 w-full sm:w-4/5 md:w-3/5 z-50" ref="el" :style="imageStyle">
+      <img src="/images/screenshot_hero.png" alt="" class="" />
+    </div>
   </div>
-      </div>
 </template>
 
 <script>
 import { ref, computed } from 'vue'
-import { MotionDirective as motion } from '@vueuse/motion'
+import { useMotion } from '@vueuse/motion'
 import { useWindowScroll } from '@vueuse/core'
 
 export default {
   setup() {
     const el = ref(null)
     const { y } = useWindowScroll()
+
+
+    useMotion(el, {
+      initial: { opacity: 0, scale: 0.5, y: 100 },
+      enter: {
+        opacity: 1,
+        y: 0,
+        scale: 1,
+        transition: {
+          delay: 1000,
+          type: 'spring',
+          stiffness: 250,
+          damping: 25,
+          mass: 0.5,
+        }
+      }
+    })
 
     const imageStyle = computed(() => ({
       transform: `translate3d(0, ${y.value * 0.1}px, -${y.value * 0.3}px) rotateX(${y.value * 0.07}deg)`,
@@ -32,10 +41,6 @@ export default {
       el,
       imageStyle,
     }
-  },
-  directives: {
-    motion: motion(),
-    /* scroll: vScroll */
   },
 }
 
