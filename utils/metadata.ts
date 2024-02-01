c65df27948deb96a0c976a9f3b2f553fb297ae0b
ndom91 <yo@ndo.dev>
2024-02-01 16:58:08 +0100

    feat: add utils

---
+++ utils/metadata.ts
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next/types';
+
+export function createMetadata(override: Metadata): Metadata {
+  return {
+    ...override,
+    openGraph: {
+      title: override.title ?? undefined,
+      description: override.description ?? undefined,
+      url: 'https://docs.briefkastenhq.com',
+      images: '/banner.png',
+      siteName: 'Briefkasten',
+      ...override.openGraph,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      creator: '@ndom91',
+      title: override.title ?? undefined,
+      description: override.description ?? undefined,
+      images: '/banner.png',
+      ...override.twitter,
+    },
+  };
+}
+
+export const baseUrl =
+  process.env.NODE_ENV === 'development'
+    ? new URL('http://localhost:3000')
+    : new URL(`https://${process.env.VERCEL_URL}`);
