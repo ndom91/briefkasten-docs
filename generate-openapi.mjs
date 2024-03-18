b2f00d8bc828eeae292c3871bee3203cd7611b05
ndom91 <yo@ndo.dev>
2024-03-18 22:15:01 +0100

    fix: update openapi generation

--- generate-openapi.mjs
+++ generate-openapi.mjs
@@ -1,10 +1,12 @@
+import { generateFiles } from "fumadocs-openapi"
+
 /**
  * @type {import("fumadocs-openapi").Config}
  * @todo Add OpenAPI Plugin to API Server
  */
-module.exports = {
+void generateFiles({
   input: ["./briefkasten.openapi.yaml"],
   output: "./content/docs",
   per: "file",
   name: () => "api-reference",
-}
+})
