c2a7b1201e4079c3347e87a06c8963caa592a358
ndom91 <yo@ndo.dev>
2024-03-19 12:13:02 +0100

    fix: update footer

--- docs/components/Footer.vue
+++ docs/components/Footer.vue
@@ -1,46 +1,22 @@
 <script setup></script>
 
 <template>
-  <footer class="body-font w-full text-gray-600">
-    <div
-      class="container mx-auto flex flex-col items-center px-5 py-8 sm:flex-row"
-    >
-      <a
-        class="title-font flex items-center justify-center font-medium text-gray-900 md:justify-start"
-      >
-        <svg
-          class="h-10 w-10 rounded-full bg-slate-800 p-2 text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          height="24"
-          fill="none"
-          aria-hidden="true"
-          stroke="#fff"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"
-          ></path>
+  <footer class="w-full text-gray-600 body-font">
+    <div class="container flex flex-col items-center py-8 px-5 mx-auto sm:flex-row">
+      <a class="flex justify-center items-center font-medium text-gray-900 md:justify-start title-font">
+        <svg class="p-2 w-10 h-10 text-white rounded-full bg-slate-800" xmlns="http://www.w3.org/2000/svg" height="24"
+          fill="none" aria-hidden="true" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          viewBox="0 0 24 24">
+          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
         </svg>
         <span class="ml-3 text-xl dark:text-zinc-100">Briefkasten</span>
       </a>
-      <p
-        class="mt-4 text-sm text-gray-500 sm:ml-4 sm:mt-0 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:pl-4"
-      >
-        © 2022 -
-        <a
-          href="https://github.com/ndom91"
-          class="ml-1 text-gray-400"
-          rel="noopener noreferrer"
-          target="_blank"
-          >@ndom91</a
-        >
+      <p class="mt-4 text-sm text-gray-500 sm:py-2 sm:pl-4 sm:mt-0 sm:ml-4 sm:border-l-2 sm:border-gray-200">
+        © 2024
+        <a href="https://github.com/ndom91" class="ml-1 text-gray-400" rel="noopener noreferrer"
+          target="_blank">@ndom91</a>
       </p>
-      <span
-        class="mt-4 inline-flex justify-center sm:ml-auto sm:mt-0 sm:justify-start"
-      >
+      <span class="inline-flex justify-center mt-4 sm:justify-start sm:mt-0 sm:ml-auto">
       </span>
     </div>
   </footer>
