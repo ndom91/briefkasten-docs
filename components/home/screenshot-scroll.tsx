6ee4ffd669e131b67cf031083c4f60631e8038f0
ndom91 <yo@ndo.dev>
2024-04-01 15:41:32 +0200

    fix: use dynamic screenshots light/dark

--- components/home/screenshot-scroll.tsx
+++ components/home/screenshot-scroll.tsx
@@ -5,12 +5,15 @@ import { useLayoutEffect } from "react"
 import { useParams } from "next/navigation"
 import Image from "next/image"
 import * as React from "react"
-import app0 from "../../public/screenshots/app0.png"
-import app1 from "../../public/screenshots/app1.png"
-import app2 from "../../public/screenshots/app2.png"
-import app3 from "../../public/screenshots/app3.png"
-import app4 from "../../public/screenshots/app4.png"
-import app5 from "../../public/screenshots/app5.png"
+import app0 from "../../public/screenshots/app0-dark.png"
+import app1 from "../../public/screenshots/app1-dark.png"
+import app1Light from "../../public/screenshots/app1-light.png"
+import app2 from "../../public/screenshots/app2-dark.png"
+import app2Light from "../../public/screenshots/app2-light.png"
+import app3 from "../../public/screenshots/app3-dark.png"
+import app3Light from "../../public/screenshots/app3-light.png"
+import app4 from "../../public/screenshots/app4-dark.png"
+import app5 from "../../public/screenshots/app5-dark.png"
 
 export const ScreenshotScroll = () => {
   function replaceState(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
@@ -49,32 +52,100 @@ export const ScreenshotScroll = () => {
         <ul>
           <li id="one">
             <article>
-              <Image src={app1} alt="Bookmarks List" />
+              <Image
+                src={app1Light}
+                className="opacity-100 transition dark:opacity-0 size-fit dark:size-0"
+                sizes="(max-width: 640px) 400px, (max-width: 960px) 500px, 1000px"
+                quality={100}
+                alt="Bookmarks List"
+                priority
+                placeholder="blur"
+              />
+              <Image
+                src={app1}
+                className="opacity-0 transition dark:opacity-100 size-0 dark:size-fit"
+                sizes="(max-width: 640px) 400px, (max-width: 960px) 500px, 1000px"
+                quality={100}
+                alt="Bookmarks List"
+                priority
+                placeholder="blur"
+              />
             </article>
           </li>
           <li id="two">
             <article>
-              <Image src={app0} alt="Settings" />
+              <Image
+                src={app0}
+                alt="Empty State"
+                className=""
+                sizes="(max-width: 640px) 400px, (max-width: 960px) 500px, 1000px"
+                quality={100}
+                placeholder="blur"
+              />
             </article>
           </li>
           <li id="three">
             <article>
-              <Image src={app2} alt="RSS Feed Reading" />
+              <Image
+                src={app2}
+                alt="Command Palette"
+                className="opacity-0 transition dark:opacity-100 size-0 dark:size-fit"
+                sizes="(max-width: 640px) 400px, (max-width: 960px) 500px, 1000px"
+                quality={100}
+                placeholder="blur"
+              />
+              <Image
+                src={app2Light}
+                alt="Command Palette"
+                className="object-cover opacity-100 transition dark:opacity-0 size-fit dark:size-0"
+                sizes="(max-width: 640px) 400px, (max-width: 960px) 500px, 1000px"
+                quality={100}
+                placeholder="blur"
+              />
             </article>
           </li>
           <li id="four">
             <article>
-              <Image src={app3} alt="Home Dashboard" />
+              <Image
+                src={app3}
+                alt="Settings Page"
+                className="opacity-0 transition dark:opacity-100 size-0 dark:size-fit"
+                sizes="(max-width: 640px) 400px, (max-width: 960px) 500px, 1000px"
+                quality={100}
+                placeholder="blur"
+              />
+              <Image
+                src={app3Light}
+                alt="Settings Page"
+                className="opacity-100 transition dark:opacity-0 size-fit dark:size-0"
+                sizes="(max-width: 640px) 400px, (max-width: 960px) 500px, 1000px"
+                quality={100}
+                placeholder="blur"
+              />
             </article>
           </li>
           <li id="five">
             <article>
-              <Image src={app4} alt="Bookmarks List with Edit Sidebar" />
+              <Image
+                src={app4}
+                alt="Empty Bookmark State - Light Mode"
+                className=""
+                sizes="(max-width: 640px) 400px, (max-width: 960px) 500px, 1000px"
+                quality={100}
+                placeholder="blur"
+              />
             </article>
           </li>
           <li id="six">
             <article>
-              <Image src={app5} alt="Light Mode" />
+              <Image
+                src={app5}
+                alt="Feeds Audio Example"
+                className=""
+                sizes="(max-width: 640px) 400px, (max-width: 960px) 500px, 1000px"
+                quality={100}
+                placeholder="blur"
+              />
             </article>
           </li>
         </ul>
